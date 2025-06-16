a3cfba2a44ac23983b6b378d30a696a34b75a6db
matheuscascao <matheusfranco23@hotmail.com>
2025-06-16 10:22:18 -0300

    chore: change package.json

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    port: 8080,
   },
-  assetsInclude: ['**/*.mp3', '**/*.wav']
+  preview: {
+    port: 8080,
+  },
+  optimizeDeps: {
+    exclude: ["lucide-react"],
+  },
+  assetsInclude: ["**/*.mp3", "**/*.wav"],
 });
